52ba68da7479cb3c4d3b446ba1e1f77011320545
EPW <erikpw009@gmail.com>
2024-10-20 00:57:45 -0700

    revert back to original files

--- src/routes/fromDBandComponents/+page.server.js
+++ src/routes/fromDBandComponents/+page.server.js
@@ -1,20 +1,16 @@
-
 import sql from '$lib/server/database';
 
 export async function load() {
-    // Await the result of the query
-    const result = await sql`
-      SELECT
-          containerNumber,
-          nameOfShip,
-          containerSize,
-          dateContainerShipped
-      FROM
-          containers
-    `;
-    
-    // Return the rows as part of the response
-    return {
-      rows: result
-    };
-  }
+    const rows = sql`
+    SELECT
+        containerNumber,
+        nameOfShip,
+        containerSize,
+        dateContainerShipped
+    FROM
+        containers`;
+
+    console.log({rows});
+
+    return { containers: rows };
+}
