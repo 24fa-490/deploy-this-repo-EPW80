719756d65669ebbb94ba63aac77da7af24092f1b
EPW <erikpw009@gmail.com>
2024-10-23 00:03:17 -0700

    debugging

--- src/lib/server/database.js
+++ src/lib/server/database.js
@@ -3,12 +3,12 @@ import { PGCONNECT } from '$env/static/private';
 
 // Ensure the environment variable exists
 if (!PGCONNECT) {
-    throw new Error('Environment variable PGCONNECT is missing.');
+	throw new Error('Environment variable PGCONNECT is missing.');
 }
 
 // Initialize the PostgreSQL connection
 const sql = postgres(PGCONNECT, {
-    // Add any additional options here, like SSL settings if needed.
+	ssl: { rejectUnauthorized: false }
 });
 
 export default sql;
